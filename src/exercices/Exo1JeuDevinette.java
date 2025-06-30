aa9c4068860cb1154cc7d7a684df76ed7b9e80ea
kernelao <oubalkass.ayoub@hotmail.com>
2025-06-29 21:54:42 -0400

    Corrections suite aux suggestions du prof

--- src/exercices/Exo1JeuDevinette.java
+++ src/exercices/Exo1JeuDevinette.java
@@ -25,22 +25,66 @@ public class Exo1JeuDevinette {
         // Initialisation de la variable qui permettra de calculer le nombre d'essais utilisés
         int essaisUtilises = 0;
 
+        // Affichage initiale de la demande
+        System.out.print("Veuillez entrer un nombre entier entre 0 et 20 inclusivement! ==>  ");
+
         // Boucle du programme, permet d'itérer jusqu'au nombre d'essais permis. Elle vérifie si l'utilisateur a obtenu le bon résultat, sinon, lui donne un indice.
         while (!success && nombreEssais != 0) {
 
             // À chaque boucle, le nombre d'essai décrémente
             nombreEssais--;
-            System.out.println("Veuillez entrer un nombre entier entre 0 et 20 inclusivement!");
-            resultat = clavier.nextInt();
+
+            // Suggestion rajoutée : Vérification de l'entrée de l'utilisateur (type valide : nombre) et de la borne (borne valide : 0à20 inclusivement)
+            if (clavier.hasNextInt()) {
+                resultat = clavier.nextInt();
+
+                // Vérification des bornes
+                while (resultat < 0 || resultat > 20) {
+                    System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entre 0 et 20 insclusivement! ==>  ");
+
+                    // Relancer une boucle en redemandant à l'utilisateur et en veillant à revérifier
+                    while (!clavier.hasNextInt()) {
+                        System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entier valide! ==>  ");
+                    }
+
+                    // On restock l'info et on reboucle pour revérifier les bornes, puis la validité du type
+                    resultat = clavier.nextInt();
+                }
+
+            } else {
+
+                // Boucle jusqu'à ce que l'utilisateur entre un entier
+                while (!clavier.hasNextInt()) {
+                    System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entier valide! ==>  ");
+                    clavier.next();
+                }
+                // Stockage du nombre entier après être sur que c'est une entrée valide
+                resultat = clavier.nextInt();
+
+                // Vérification des bornes
+                while (resultat < 0 || resultat > 20) {
+                    System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entre 0 et 20 insclusivement! ==>  ");
+
+                    // Relancer une boucle en redemandant à l'utilisateur et en veillant à revérifier
+                    while (!clavier.hasNextInt()) {
+                        System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entier valide! ==>  ");
+                        clavier.next();
+                    }
+
+                    // On restock l'info et on reboucle pour revérifier les bornes, puis la validité du type
+                    resultat = clavier.nextInt();
+                }
+
+            }
 
             // Condition sur une valeur inférieur au résultat attendu
             if (resultat < nombreAleatoire) {
 
                 // Condition secondaire qui vérifie s'il reste encore un nombre d'essai afin d'éviter un message d'indice alors que le jeu est sur le point de s'arrêter
                 if (nombreEssais == 0) {
-                    System.out.println("Désolé, vous avez perdu !");
+                    System.out.print("Désolé, vous avez perdu !");
                 } else {
-                    System.out.println("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre plus grand que " + resultat + ", mais inférieur ou égal à 20.");
+                    System.out.print("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre plus\ngrand que " + resultat + ", mais inférieur ou égal à 20. ==>  ");
                 }
 
             // Condition sur une valeur inférieur au résultat attendu
@@ -48,16 +92,16 @@ public class Exo1JeuDevinette {
 
                 // Condition secondaire qui vérifie s'il reste encore un nombre d'essai afin d'éviter un message d'indice alors que le jeu est sur le point de s'arrêter
                 if (nombreEssais == 0) {
-                    System.out.println("Désolé, vous avez perdu !");
+                    System.out.print("Désolé, vous avez perdu !");
                 } else {
-                    System.out.println("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre plus petit que " + resultat + ", mais supérieur ou égal à 0.");
+                    System.out.print("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre plus\npetit que " + resultat + ", mais supérieur ou égal à 0. ==>  ");
                 }
 
             // Sinon, si c'est ni inférieur, ni supérieur, alors l'utilisateur a deviné le résultat
             } else {
                 // Calcul du nombre d'essais utilisés (5 - nombre d'essais restants = nombre d'essais utilisés)
                 essaisUtilises = NOMBRE_TENTATIVES_AUTORISÉES - nombreEssais;
-                System.out.println("Vous avez bien deviné le nombre, en " + essaisUtilises + " essais. Bravo!");
+                System.out.print("Vous avez bien deviné le nombre, en " + essaisUtilises + " essais. Bravo!");
 
                 // Variable d'état qui change pour ne plus refaire de boucle
                 success=true;
--- src/exercices/Exo1JeuDevinetteDoWhile.java
+++ src/exercices/Exo1JeuDevinetteDoWhile.java
@@ -25,21 +25,59 @@ public class Exo1JeuDevinetteDoWhile {
         // Initialisation de la variable qui permettra de calculer le nombre d'essais utilisés
         int essaisUtilises = 0;
 
+        // Affichage initiale de la demande
+        System.out.print("Veuillez entrer un nombre entier entre 0 et 20 inclusivement! ==>  ");
+
         // Boucle du programme, permet d'itérer jusqu'au nombre d'essais permis. Elle vérifie si l'utilisateur a obtenu le bon résultat, sinon, lui donne un indice.
         do {
             // À chaque boucle, le nombre d'essai décrémente
             nombreEssais--;
-            System.out.println("Veuillez entrer un nombre entier entre 0 et 20 inclusivement!");
-            resultat = clavier.nextInt();
+
+            // Suggestion rajoutée v2 : Vérification de l'entrée de l'utilisateur (type valide : nombre) et de la borne (borne valide : 0à20 inclusivement)
+//            if (clavier.hasNextInt()) {
+//                resultat = clavier.nextInt();
+//            } else {
+//                while (!clavier.hasNextInt()) {
+//                    System.out.print("Veuillez entrer un nombre entier entre 0 et 20 inclusivement! ==>  ");
+//                    clavier.next();
+//                }
+//                resultat = clavier.nextInt();
+//            }
+
+            // Il faudrait une boucle qui vérifie plusieurs condition et si l'une n'est pas respecté, on recommence depuis le début
+            /* Le continue est adapté dans cette situation, si une condition n'est pas bonne (on capte l'erreur), alors on affiche
+            un message à l'utilisateur, on utilise le "continue" pour ignorer tout le code de la boucle et directement recommencer la
+            boucle car on a true. Si tous nos vérifications sont bonne, donc on rentre pas dans les conditions, alors on peut arriver
+            à "break" pour ne plus reboucler. On aura obtenu la bonne réponse.*/
+
+            while (true) {
+
+                // On vérifie si c'est un nombre entier
+                if (!clavier.hasNextInt()) {
+                    System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entier valide! ==>  ");
+                    clavier.next();
+                    continue;
+                }
+
+                resultat = clavier.nextInt();
+
+                // On vérifie si la valeur est comprise entre 0 et 20
+                if (resultat < 0 || resultat > 20) {
+                    System.out.print("Désolé, mais la valeur que vous venez d'entrer n'est pas acceptée!\nVeuillez entrer un nombre entre 0 et 20 insclusivement! ==>  ");
+                    continue;
+                }
+
+                break;
+            }
 
             // Condition sur une valeur inférieure au résultat attendu
             if (resultat < nombreAleatoire) {
 
                 // Condition secondaire qui vérifie s'il reste encore un nombre d'essai afin d'éviter un message d'indice alors que le jeu est sur le point de s'arrêter
                 if (nombreEssais == 0) {
-                    System.out.println("Désolé, vous avez perdu !");
+                    System.out.print("Désolé, vous avez perdu !");
                 } else {
-                    System.out.println("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre plus grand que " + resultat + ", mais inférieur ou égal à 20.");
+                    System.out.print("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre \nplus grand que " + resultat + ", mais inférieur ou égal à 20. ==>  ");
                 }
 
                 // Condition sur une valeur inférieure au résultat attendu
@@ -47,16 +85,16 @@ public class Exo1JeuDevinetteDoWhile {
 
                 // Condition secondaire qui vérifie s'il reste encore un nombre d'essai afin d'éviter un message d'indice alors que le jeu est sur le point de s'arrêter
                 if (nombreEssais == 0) {
-                    System.out.println("Désolé, vous avez perdu !");
+                    System.out.print("Désolé, vous avez perdu !");
                 } else {
-                    System.out.println("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre plus petit que " + resultat + ", mais supérieur ou égal à 0.");
+                    System.out.print("Incorrect, il vous reste " + nombreEssais + " essais. Veuillez entrer un nombre \nplus petit que " + resultat + ", mais supérieur ou égal à 0. ==>  ");
                 }
 
                 // Sinon, si c'est ni inférieur, ni supérieur, alors l'utilisateur a deviné le résultat
             } else {
                 // Calcul du nombre d'essais utilisés (5 - nombre d'essais restants = nombre d'essais utilisés)
                 essaisUtilises = NOMBRE_TENTATIVES_AUTORISÉES - nombreEssais;
-                System.out.println("Vous avez bien deviné le nombre, en " + essaisUtilises + " essais. Bravo!");
+                System.out.print("Vous avez bien deviné le nombre, en " + essaisUtilises + " essais. Bravo!");
 
                 // Variable d'état qui change pour ne plus refaire de boucle
                 success=true;
